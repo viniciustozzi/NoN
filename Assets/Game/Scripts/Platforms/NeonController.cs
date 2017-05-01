cccda62bf477654c4b530fddd90ebb94cae60e6f
DESKTOP-87VQ797\Waldir Jr <waldirn.rodrigues@gmail.com>
2017-05-01 03:25:45 -0300

    Nem sei o que eu fiz

---
+++ Assets/Game/Scripts/Platforms/NeonController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeonController : MonoBehaviour {
+    
+    private SpriteRenderer[] sprites;
+    private bool turnOn, turnOff;
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+
+        sprites = GetComponentsInChildren<SpriteRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+    public void TurnOn(float speed)
+    {
+        this.speed = speed;
+        turnOn = true;
+        turnOff = false;
+    }
+
+    public void TurnOff(float speed)
+    {
+        this.speed = speed;
+        turnOff = true;
+    }
+}
