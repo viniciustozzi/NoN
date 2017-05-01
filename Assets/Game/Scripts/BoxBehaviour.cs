3f1baecfd87b906a197ce03b6f4a4bedb782106f
viniciustozzi <tozzivinicius@gmail.com>
2017-05-01 13:12:38 -0300

    Adicionado som de arrastar a caixa

---
+++ Assets/Game/Scripts/BoxBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxBehaviour : MonoBehaviour
+{
+
+    public AudioClip dragSound;
+
+    private AudioSource mAudioSource;
+
+    private void Start()
+    {
+        mAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            if (!mAudioSource.isPlaying)
+            {
+                mAudioSource.clip = dragSound;
+                mAudioSource.Play();
+            }
+        }
+    }
+}
