28fde91fd4f6895933561871e630567fe588be11
DESKTOP-87VQ797\Waldir Jr <waldirn.rodrigues@gmail.com>
2017-05-01 10:51:27 -0300

    Walk animation updated

--- Assets/Game/Scripts/Platforms/BlinkPlatformBehaviour.cs
+++ Assets/Game/Scripts/Platforms/BlinkPlatformBehaviour.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class BlinkPlatformBehaviour : MonoBehaviour {
 
+    public GameObject[] platforms;
+    public float timeToChange;
+    public int index;
+
+    private float time;
+    
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        time = 0;
+
+        index = 0;
+        
+        foreach (GameObject g in platforms)
+        {
+            g.SetActive(false);
+        }
+
+        platforms[index].SetActive(true);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        time += Time.deltaTime;
+
+        if(time > timeToChange)
+        {
+            platforms[index].SetActive(false);
+            index++;
+
+            if (index >= platforms.Length)
+                index = 0;
+
+            platforms[index].SetActive(true);
+
+            time = 0;
+        }
 	}
 }
--- Assets/Game/Scripts/Player/PlayerController.cs
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -75,7 +75,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.transform.tag == "Platform")
         {
