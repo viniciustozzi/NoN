529fcd2ffafad6a4a2cf62b73f0d5d7ca729ccea
viniciustozzi <tozzivinicius@gmail.com>
2017-05-01 15:15:01 -0300

    Animação de pulo aplicada

--- Assets/Game/Scripts/Player/PlayerController.cs
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -32,6 +32,8 @@ public class PlayerController : MonoBehaviour
         m_anim.SetFloat("Speed", m_rig.velocity.magnitude);
         m_anim.SetBool("Jump", isJumping);
 
+        Debug.Log(isJumping);
+
         if (hAxis < 0 && !facingRight)
             Flip();
         else if (hAxis > 0 && facingRight)
@@ -44,7 +46,6 @@ public class PlayerController : MonoBehaviour
                 mAudioSource.clip = walkingSound;
                 mAudioSource.Play();
             }
-
         }
 
         if (Input.GetButtonDown("Jump") && CheckGround())
@@ -56,15 +57,15 @@ public class PlayerController : MonoBehaviour
     private bool CheckGround()
     {
         return Physics2D.Linecast(transform.position, groundCheck.position, layer);
-        
     }
 
     private void Jump()
     {
+        isJumping = true;
         transform.SetParent(null);
         m_rig.velocity = new Vector2(m_rig.velocity.x, jumpForce);
-        isJumping = true;
     }
+
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -84,8 +85,11 @@ public class PlayerController : MonoBehaviour
         {
             transform.SetParent(collision.transform);
         }
+    }
 
-        if (collision.transform.tag == "Ground")
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Ground" || collision.transform.tag == "Platform")
         {
             isJumping = false;
         }
@@ -93,6 +97,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
+
         if (collision.transform.tag == "Platform")
         {
             transform.SetParent(null);
